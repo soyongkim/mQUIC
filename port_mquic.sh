c7586e32bfa468ee4580c8ae7d257c3dc65e9c34
soyongkim <thdyd324@gmail.com>
2023-02-17 21:25:54 +0900

    update port_mquic.sh

--- port_mquic.sh
+++ port_mquic.sh
@@ -20,7 +20,7 @@ echo "port quic_handover_module"
 rsync ./net/third_party/quiche/src/quic/core/crypto/tls_connection.* ../net/third_party/quiche/src/quic/core/crypto
 rsync ./net/third_party/quiche/src/quic/core/quic_connection.* ./net/third_party/quiche/src/quic/core/quic_one_block_arena.h ./net/third_party/quiche/src/quic/core/quic_framer.* ./net/third_party/quiche/src/quic/core/quic_path_validator.* ./net/third_party/quiche/src/quic/core/quic_session.* ./net/third_party/quiche/src/quic/core/quic_udp_socket_posix.cc ./net/third_party/quiche/src/quic/core/quic_sent_packet_manager.* ../net/third_party/quiche/src/quic/core
 rsync ./net/third_party/quiche/src/quic/core/congestion_control/pacing_sender.* ../net/third_party/quiche/src/quic/core/congestion_control/
-rsync ./net/third_party/quiche/src/quic/tools/quic_client_base.* ./net/third_party/quiche/src/quic/tools/quic_toy_client.* ./net/third_party/quiche/src/quic/tools/quic_client_epoll_network_helper.* ../net/third_party/quiche/src/quic/tools
+rsync ./net/third_party/quiche/src/quic/tools/quic_client_base.* ./net/third_party/quiche/src/quic/tools/quic_toy_client.* ./net/third_party/quiche/src/quic/tools/quic_spdy_server_base.* ./net/third_party/quiche/src/quic/tools/quic_client_epoll_network_helper.* ../net/third_party/quiche/src/quic/tools
 
 rsync ./net/third_party/quiche/src/quic/core/quic_dispatcher.* ../net/third_party/quiche/src/quic/core
 rsync ./net/third_party/quiche/src/quic/tools/quic_toy_server.* ./net/third_party/quiche/src/quic/tools/quic_server.* ../net/third_party/quiche/src/quic/tools
