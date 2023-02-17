7d910a0f01b885fe857c3a55d0568e693699b985
soyongkim <thdyd324@gmail.com>
2023-02-17 20:37:54 +0900

    update cwnd func

--- port_module.sh
+++ port_module.sh
@@ -9,6 +9,9 @@ then
     rsync ../net/third_party/quiche/src/quic/core/congestion_control/pacing_sender.* net_backup/third_party/quiche/src/quic/core/congestion_control
     rsync ../net/third_party/quiche/src/quic/core/crypto/tls_connection.* net_backup/third_party/quiche/src/quic/core/crypto
     rsync ../net/third_party/quiche/src/quic/tools/quic_client_base.* ../net/third_party/quiche/src/quic/tools/quic_toy_client.* ../net/third_party/quiche/src/quic/tools/quic_client_epoll_network_helper.* net_backup/third_party/quiche/src/quic/tools
+
+    rsync ./net/third_party/quiche/src/quic/core/quic_dispatcher.* ../net/third_party/quiche/src/quic/core
+    rsync ./net/third_party/quiche/src/quic/tools/quic_toy_server.* ./net/third_party/quiche/src/quic/tools/quic_server.* ../net/third_party/quiche/src/quic/tools
 fi
 
 
@@ -17,3 +20,6 @@ rsync ./net/third_party/quiche/src/quic/core/crypto/tls_connection.* ../net/thir
 rsync ./net/third_party/quiche/src/quic/core/quic_connection.* ./net/third_party/quiche/src/quic/core/quic_one_block_arena.h ./net/third_party/quiche/src/quic/core/quic_framer.* ./net/third_party/quiche/src/quic/core/quic_path_validator.* ./net/third_party/quiche/src/quic/core/quic_session.* ./net/third_party/quiche/src/quic/core/quic_udp_socket_posix.cc ./net/third_party/quiche/src/quic/core/quic_sent_packet_manager.* ../net/third_party/quiche/src/quic/core
 rsync ./net/third_party/quiche/src/quic/core/congestion_control/pacing_sender.* ../net/third_party/quiche/src/quic/core/congestion_control/
 rsync ./net/third_party/quiche/src/quic/tools/quic_client_base.* ./net/third_party/quiche/src/quic/tools/quic_toy_client.* ./net/third_party/quiche/src/quic/tools/quic_client_epoll_network_helper.* ../net/third_party/quiche/src/quic/tools
+
+rsync ./net/third_party/quiche/src/quic/core/quic_dispatcher.* ../net/third_party/quiche/src/quic/core
+rsync ./net/third_party/quiche/src/quic/tools/quic_toy_server.* ./net/third_party/quiche/src/quic/tools/quic_server.* ../net/third_party/quiche/src/quic/tools
--- rollback.sh
+++ rollback.sh
@@ -8,4 +8,7 @@ then
     rsync ./net_backup/third_party/quiche/src/quic/core/quic_connection.* ./net_backup/third_party/quiche/src/quic/core/quic_one_block_arena.h ./net_backup/third_party/quiche/src/quic/core/quic_framer.* ./net_backup/third_party/quiche/src/quic/core/quic_path_validator.* ./net_backup/third_party/quiche/src/quic/core/quic_session.* ./net_backup/third_party/quiche/src/quic/core/quic_udp_socket_posix.cc ./net_backup/third_party/quiche/src/quic/core/quic_sent_packet_manager.* ../net/third_party/quiche/src/quic/core
     rsync ./net_backup/third_party/quiche/src/quic/core/congestion_control/pacing_sender.* ./net_backup/third_party/quiche/src/quic/core/congestion_control/
     rsync ./net_backup/third_party/quiche/src/quic/tools/quic_client_base.* ./net_backup/third_party/quiche/src/quic/tools/quic_toy_client.* ./net_backup/third_party/quiche/src/quic/tools/quic_client_epoll_network_helper.* ../net/third_party/quiche/src/quic/tools
+
+    rsync ./net_backup/third_party/quiche/src/quic/core/quic_dispatcher.* ../net/third_party/quiche/src/quic/core
+    rsync ./net_backup/third_party/quiche/src/quic/tools/quic_toy_server.* ./net/third_party/quiche/src/quic/tools/quic_server.* ../net/third_party/quiche/src/quic/tools
 fi
